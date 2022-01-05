b6f5b11d1398f7aad833a16dfba347a3fc813585
RayanElChakik <rayan.elchakik@gmail.com>
2022-01-05 12:33:45 +0000

    Step 3

--- index.js
+++ index.js
@@ -8,5 +8,25 @@ app.get('/',(req,res) =>{
     res.send('OK!')
 })
 
+
+// Creating a route when /test invoked it sends a status and a message 
+app.get('/test',(req,res) =>{
+    console.log('Testing Server route')
+    res.json({status:200, message:"ok"})
+})
+
+// Creating a route when /time invoked it sends status and a time message
+let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+let time = hours + ":" + minutes + ":" + seconds;
+
+app.get('/time',(req,res) =>{
+    console.log('Testing Server route')
+    res.json({status:200, message:time})
+})
+
+
 // Sever Creation that listens to port= 3000
 app.listen(3000)
