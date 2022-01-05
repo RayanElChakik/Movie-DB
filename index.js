d5654c1d6290d62d789fb4997eceae2d3251da81
RayanElChakik <rayan.elchakik@gmail.com>
2022-01-05 12:35:42 +0000

    Step 4

--- index.js
+++ index.js
@@ -27,6 +27,29 @@ app.get('/time',(req,res) =>{
     res.json({status:200, message:time})
 })
 
+// Creating a route when /hello <ID> invoked it sends status and a message with ID passed 
+app.get('/hello/:ID',(req,res) =>{
+    console.log('Testing Server route')
+        res.json({status:200, message:`Hello, ${req.params.ID}`})
+})
+
+app.get('/hello',(req,res)=>{
+    console.log('Testing Server route')
+    res.json({status:200, message:"Hello"})
+})
+
+// Creating a route when /search?s=<SEARCH>  invoked with some conditions 
+app.get('/search',(req,res) =>{
+    let search = req.query.s;
+    if(search){
+        console.log('Server route with search parameter have been passed')
+        res.status(200).json({status:200, message:'Ok', data:{search}})
+    }else{
+        console.log('Testing Server Route')
+        res.status(500).json({status:500, error:true, message:"You have to provide a serach"})
+    }
+})
+
 
 // Sever Creation that listens to port= 3000
 app.listen(3000)
